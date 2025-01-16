d75649c6c7116b31d2317009e4b952a709a69292
rishabh <rishabhkumar900632gmail.com>
2025-01-17 01:11:34 +0530

    realtime meassages send and getting previous messages

--- client/src/pages/chat/components/chat-container/components/message-bar/index.jsx
+++ client/src/pages/chat/components/chat-container/components/message-bar/index.jsx
@@ -1,3 +1,5 @@
+import { useSocket } from '@/context/SocketContext'
+import { useAppStore } from '@/store'
 import EmojiPicker from 'emoji-picker-react'
 import React, { useEffect, useRef, useState } from 'react'
 import {GrAttachment} from "react-icons/gr"
@@ -7,6 +9,8 @@ const MessageBar = () => {
     const emojiRef=useRef();
     const [message, setMessage] = useState("")
     const [emojiPickerOpen, setEmojiPickerOpen] = useState(false);
+    const {selectedChatType,selectedChatData,userInfo} =useAppStore();
+    const socket=useSocket();
 
     useEffect(()=>{
         function handleClickOutSide(event){
@@ -24,7 +28,16 @@ const MessageBar = () => {
         setMessage((msg)=>msg + emoji.emoji)
     }
     const handleSendMessage=async ()=>{
-        
+        if(selectedChatType==="contact"){
+            socket.emit("sendMessage",{
+                sender:userInfo.id,
+                content:message,
+                recipient:selectedChatData._id,
+                messageType:"text",
+                fileUrl:undefined,
+            });
+            setMessage("")
+        }
     }
   return (
     <div className='h-[10vh] bg-[#1c1d25] flex justify-center items-center px-8 mb-5 gap-6 '>
